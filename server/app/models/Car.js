bece24beab2646bea3d19b6b80da31b7fbf112ea
tilucass <ti.novastecnologias@gmail.com>
2021-08-26 17:52:42 -0300

    update: teste schema

--- server/app/models/Car.js
+++ server/app/models/Car.js
@@ -6,7 +6,12 @@ const Car = sequelize.define("car", {
   model: DataTypes.STRING,
   brand: DataTypes.STRING,
   hp: DataTypes.INTEGER,
+}, {
+  schema: "admin"
 });
+
+// new schema is created
+sequelize.createSchema('admin').then(() => {});
 
 //create table if not exists...
 const init = async () => {
--- server/app/routers/routes.js
+++ server/app/routers/routes.js
@@ -1,16 +1,16 @@
 const express = require("express"); 
-const AuthController = require("../controllers/AuthController");
+//const AuthController = require("../controllers/AuthController");
 const CarsController = require("../controllers/Cars");
 const router = express.Router();
 
 
 router.post("/login", AuthController.login);
 
-router.get("/cars", AuthController.auth, CarsController.all);
-router.post("/cars", AuthController.auth, CarsController.create);
+router.get("/cars", CarsController.all);
+router.post("/cars", CarsController.create);
 
-router.get("/secure", AuthController.auth, (req, res) => {
-    res.json({ message: "this is a secure route!", username: req.username });
-  });
+// router.get("/secure", AuthController.auth, (req, res) => {
+//     res.json({ message: "this is a secure route!", username: req.username });
+//   });
 
 module.exports = router;
